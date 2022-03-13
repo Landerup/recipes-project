57fc5e6179267b12f7f0a758385c0405b93ab4ac
RasmusLanderup <Rasmus.landerup@live.se>
2022-03-13 15:41:28 +0100

    hompage layout design

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,1 +1,15 @@
 @extends('layouts.app')
+
+@section('content')
+<div class="container-lg">
+    <div class="row">
+        <div class="col-5 bg-primary m-3">column</div>
+        <div class="col-5 bg-warning m-3">column</div>
+        <div class="col-5 bg-warning m-3">column</div>
+        <div class="col-5 bg-warning m-3">column</div>
+        <div class="col-5 bg-warning m-3">column</div>
+        <div class="col-5 bg-warning m-3">column</div>
+        <div class="col-5 bg-warning m-3">column</div>
+    </div>
+</div>
+@endsection
