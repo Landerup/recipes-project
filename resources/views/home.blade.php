943e7d8849607d722003f74a11aa5e40fdc2cdad
RasmusLanderup <Rasmus.landerup@live.se>
2022-03-13 16:30:58 +0100

    added layout for recipe-page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,9 +3,8 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+        <div class="col-md-3 shadow border border-success rounded">
+            <div class="">
 
                 <div class="card-body">
                     @if (session('status'))
@@ -14,9 +13,47 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    @auth
+                        <p class="text-center">{{ auth()->user()->name }}</p>
+                    @endauth
+
                 </div>
             </div>
+        </div>
+    </div>
+</div>
+<div class="container my-4">
+    <div class="row justify-content-center">
+        <div class="col-4 mx-3">
+            <div class="row">
+                <div class="col my-3 bg-primary">Title</div>
+            </div>
+            <div class="row">
+                <div class="co my-3 bg-info">Ingredients
+                    <div class="my-2 bg-success">
+                        <ul>
+                            <li>Banana</li>
+                            <li>Äpple</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col my-3 bg-danger">Instructions
+                    <div class="my-2 bg-success">
+                        <ol>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                        </ol>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-7 mx-3 bg-warning">Picture
+            <div class="col my-3 bg-success">Rating</div>
         </div>
     </div>
 </div>
