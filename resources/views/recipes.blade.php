0dd0de7aaefcda210a30fdc64d69ea8f1aef4f5e
Benradic <benjaminradic9@gmail.com>
2022-03-14 11:32:47 +0100

    testar

--- resources/views/recipes.blade.php
+++ resources/views/recipes.blade.php
@@ -44,7 +44,7 @@
                     <div class="my-2 bg-success">
                         <ol>
                             <li>Test</li>
-                            <li></li>
+                            <li>Hej</li>
                             <li></li>
                             <li></li>
                         </ol>
