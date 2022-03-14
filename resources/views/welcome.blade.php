5c8e20195f92b90c359bd7b3e66b70fec2564a8b
RasmusLanderup <Rasmus.landerup@live.se>
2022-03-14 11:12:10 +0100

    small layout fixes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,13 +3,10 @@
 @section('content')
 <div class="container-lg">
     <div class="row">
-        <div class="col-5 bg-primary m-3">column</div>
-        <div class="col-5 bg-warning m-3">column</div>
-        <div class="col-5 bg-warning m-3">column</div>
-        <div class="col-5 bg-warning m-3">column</div>
-        <div class="col-5 bg-warning m-3">column</div>
-        <div class="col-5 bg-warning m-3">column</div>
-        <div class="col-5 bg-warning m-3">column</div>
+        <div class="col-5 bg-primary m-3">Recipe Post</div>
+        <div class="col-5 bg-warning m-3">Recipe Post</div>
+        <div class="col-5 bg-warning m-3">Recipe Post</div>
+        <div class="col-5 bg-primary m-3">Recipe Post</div>
     </div>
 </div>
 @endsection
