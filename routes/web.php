afa5048f770bafa9e72f487b79c334cfebacf8f8
RasmusLanderup <Rasmus.landerup@live.se>
2022-03-14 13:39:09 +0100

    merged homepage

--- routes/web.php
+++ routes/web.php
@@ -19,8 +19,4 @@ Route::get('/', function () {
 
 Auth::routes();
 
-<<<<<<< HEAD
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-=======
 Route::get('/profile', [App\Http\Controllers\HomeController::class, 'index'])->name('profile');
->>>>>>> homepage
