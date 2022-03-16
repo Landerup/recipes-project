c5a25582a7196c2abe96144cfa88ad3a23cc9e64
RasmusLanderup <Rasmus.landerup@live.se>
2022-03-16 15:03:13 +0100

    merged with add-recipe

--- routes/web.php
+++ routes/web.php
@@ -24,9 +24,5 @@ Auth::routes();
 Route::get('/profile', [App\Http\Controllers\HomeController::class, 'index'])->name('profile');
 
 Route::get('/recipes', [App\Http\Controllers\RecipeController::class, 'index'])->name('recipes');
-<<<<<<< HEAD
-=======
-
 
 Route::resource('/recipe', App\Http\Controllers\RecipeController::class);
->>>>>>> add-recipes
